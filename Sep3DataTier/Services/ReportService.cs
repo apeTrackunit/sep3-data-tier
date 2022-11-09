c8e2eeab8e679f1c1a250a721181c68c3a1ad65a
apeTrackunit <ape@trackunit.com>
2022-11-09 17:10:46 +0100

    Init data

--- Sep3DataTier/Services/ReportService.cs
+++ Sep3DataTier/Services/ReportService.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using Google.Protobuf;
 using Model;
 using Grpc.Core;
 using TempData;
@@ -10,7 +12,24 @@ public class ReportService : Report.ReportBase
 
     public override Task<ReportList> GetReports(ReportFilter request, ServerCallContext context)
     {
+
         ICollection<ReportObject> data = new List<ReportObject>();
+        ReportObject obj = new ReportObject
+        {
+            Date = "2020-01-01",
+            Description = "govno",
+            Location = new LocationObject
+            {
+                Latitude = 89.01023,
+                Longitude = 12.00213,
+                Size = 2,
+            },
+            Proof = ByteString.CopyFrom("Hello world", Encoding.Default),
+            Status = "Done",
+            Time = "12:12:12"
+        };
+        
+        data.Add(obj);
         
         return Task.FromResult(new ReportList 
         {
