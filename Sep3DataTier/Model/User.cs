513f04186207c2f5f21187aef298fbcd9392cc3b
kegliitis <eglitiskristians@gmail.com>
2022-11-23 10:26:07 +0100

    Proto stuff

---
+++ Sep3DataTier/Model/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Model;
+
+public class User
+{
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public User(string email, string username, string password)
+    {
+        Email = email;
+        Username = username;
+        Password = password;
+    }
+}
