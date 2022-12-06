be4f74629c9486fb98246bc2a40868f4d7d1c159
András Kovács <k.andras9@gmail.com>
2022-12-06 23:35:55 +0100

    new migrations added to create event table
    Event.cs created, it has a user and a report and other fields
    
    EventService createevent returns an actual grpc object
    
    DatabaseContext has new DbSet of Events

---
+++ Sep3DataTier/Migrations/20221206174607_InitEvent.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sep3DataTier.Migrations
+{
+    public partial class InitEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateOnly = table.Column<DateOnly>(type: "date", nullable: false),
+                    TimeOnly = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    Proof = table.Column<byte[]>(type: "bytea", nullable: true),
+                    OrganiserId = table.Column<string>(type: "text", nullable: true),
+                    ReportId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_AspNetUsers_OrganiserId",
+                        column: x => x.OrganiserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Events_Reports_ReportId",
+                        column: x => x.ReportId,
+                        principalTable: "Reports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            // migrationBuilder.InsertData(
+            //     table: "AspNetRoles",
+            //     columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+            //     values: new object[,]
+            //     {
+            //         { "4540e966-9173-4899-94aa-cdc09727f861", "8562d7f3-e7b1-4175-ac22-16a930ab5c0e", "User", "USER" },
+            //         { "4bab06a0-adf6-4963-835e-609f4a21fc9f", "e800b944-abf1-4393-b24d-102ecdffc82e", "Admin", "ADMIN" }
+            //     });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganiserId",
+                table: "Events",
+                column: "OrganiserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ReportId",
+                table: "Events",
+                column: "ReportId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            // migrationBuilder.DeleteData(
+            //     table: "AspNetRoles",
+            //     keyColumn: "Id",
+            //     keyValue: "4540e966-9173-4899-94aa-cdc09727f861");
+            //
+            // migrationBuilder.DeleteData(
+            //     table: "AspNetRoles",
+            //     keyColumn: "Id",
+            //     keyValue: "4bab06a0-adf6-4963-835e-609f4a21fc9f");
+        }
+    }
+}
--- Sep3DataTier/Repository/DatabaseContext.cs
+++ Sep3DataTier/Repository/DatabaseContext.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Model;
 using Sep3DataTier.Repository;
 
 namespace Sep3DataTier.Database;
 
-public class DatabaseContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+public class DatabaseContext : DbContext
 {
     public DbSet<Location> Locations { get; set; }
     public DbSet<Model.Report> Reports { get; set; }
-    
+
     public DbSet<ApplicationUser> Users { get; set; }
     
+    public DbSet<Model.Event> Events { get; set; }
+
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
         
@@ -35,6 +36,7 @@ public class DatabaseContext : IdentityDbContext<ApplicationUser, IdentityRole, 
         modelBuilder.Entity<Model.Report>().HasKey(report => report.Id);
         modelBuilder.Entity<ApplicationUser>().HasKey(user => user.Id);
         modelBuilder.Entity<ApplicationUser>().HasIndex(user => user.Email).IsUnique();
+        modelBuilder.Entity<Model.Event>().HasKey(e => e.Id);
         modelBuilder.Entity<IdentityRole>().HasData(GenerateIdentityRole("User"));
         modelBuilder.Entity<IdentityRole>().HasData(GenerateIdentityRole("Admin"));
         base.OnModelCreating(modelBuilder);
